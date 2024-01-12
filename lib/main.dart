071849af5f99eed1df59e676caef0357339cbcd0
Ahsan Ashfaq <aashfaq@stellarminore.com>
2024-01-12 19:05:07 +0500

    Update main.dart to support splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  runApp(const App());
+
+  Future.delayed(const Duration(seconds: 5), () {
+    FlutterNativeSplash.remove();
+  });
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('Home'),
         ),
       ),
     );
